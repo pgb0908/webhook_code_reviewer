c5496f9269ef33e697376ffdff21cbfb9868ecb6
gyubong_park <gyubong_park@tmaxsoft.com>
2026-03-06 16:57:17 +0900

    Add copyright headers for all modules to comply with licensing requirements

--- gitlab/client.py
+++ gitlab/client.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2026 TmaxSoft Co., Ltd.
+# All rights reserved.
+#
+# This software is the confidential and proprietary information of TmaxSoft Co., Ltd. ("Confidential Information").
+# You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement you entered into with TmaxSoft Co., Ltd.
+
 import logging
 import requests
 
